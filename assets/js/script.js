374bcec493923ab737659f128ccd54d74844c313
LucasAdao <l.adao@outlook.com>
2024-08-06 18:16:41 -0300

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function linkedinLink() {
+    window.location.href = 'https://www.linkedin.com/in/l-ucas/';
+}
+function githubLink(){
+    window.location.href = "https://github.com/LucasAdao";
+}
+function instagramLink(){
+    window.location.href = "https://www.instagram.com/lucass.adao/";
+}
