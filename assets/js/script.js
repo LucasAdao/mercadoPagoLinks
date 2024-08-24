9f09a96bed866e523ed2e6e62a956ab8e7a6055f
LucasAdao <l.adao@outlook.com>
2024-08-23 21:27:24 -0300

    teste

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-function linkedinLink() {
-    window.location.href = 'https://www.linkedin.com/in/l-ucas/';
-}
-function githubLink(){
-    window.location.href = "https://github.com/LucasAdao";
+
+function whatsappLink(){
+    window.location.href = "https://wa.me/5508199719070?text=Oi%20Lucas%2C%20tenho%20interesse%20no%20seu%20produto%2C%20pode%20vim%20aqui%20para%20gente%20fazer%20uma%20simula%C3%A7%C3%A3o%3F";
 }
 function instagramLink(){
-    window.location.href = "https://www.instagram.com/lucass.adao/";
+    window.location.href = "https://www.instagram.com/lucasferreiramercadopago/";
 }
